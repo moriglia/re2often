3740b57948c251997514a96bc151291894ab8d18
Marco Origlia <marco.origlia@santannapisa.it>
2024-11-14 19:54:26 +0100

    Fix scale of Gaussian random number generation routine

--- src/fortran/simtools/sim_direct_channel.f90
+++ src/fortran/simtools/sim_direct_channel.f90
@@ -53,7 +53,7 @@ contains
     real(wp), allocatable :: llr(:)
     real(wp), allocatable :: llr_updated(:)
 
-    real(wp) :: N0, N0_half
+    real(wp) :: N0, N0_half, sigma
 
     integer :: new_errors, n_ldpc_it
 
@@ -103,13 +103,14 @@ contains
     snr_loop : do i_snr = 1 , Nsnr
        N0 = 10.0_wp**(-snrdb_array(i_snr)/10.0_wp) * alphabet%variance
        N0_half = 0.5_wp * N0
+       sigma = sqrt(N0_half)
        
        frame_loop : do i_frame = 1, max_loops
           call alphabet%random_symbol(x)
           word = alphabet%symbol_to_grey_array(x)
           synd = decoder%word_to_synd(word)
           
-          y = rvs_normal(loc=alphabet%symbol_index_to_real(x), scale=N0_half)
+          y = rvs_normal(loc=alphabet%symbol_index_to_real(x), scale=sigma)
           llr = y_to_llr_grey_array(N0, alphabet, y)
 
           n_ldpc_it = ldpc_iterations
