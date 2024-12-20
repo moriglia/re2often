a42e763e630be540337cf9c15290357662ba1686
Marco Origlia <marco.origlia@santannapisa.it>
2024-12-20 11:36:29 +0100

    Add binary search function

---
+++ src/re2often_utils.f90
@@ -0,0 +1,58 @@
+! SPDX-License-Identifier: GPL-3.0-or-later
+! Copyright (C) 2024  Marco Origlia
+
+!    This program is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+
+!    This program is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+
+!    You should have received a copy of the GNU General Public License
+!    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+module re2often_utils
+    !! author: Marco Origlia
+    !! license: GPL-3.0-or-later
+    implicit none
+
+
+contains
+    pure function binsearch(vector, y) result (index)
+        !! Retrieve index of datum within a vector
+        double precision, intent(in) :: vector(:)
+        !! 1-based array \(\mathbf{v}\) to search data from
+        double precision, intent(in) :: y
+        !! Datum \(y\) to search
+        integer :: index
+        !! `index` \(i\) such that \(\mathbf{v}_i \leq y \lt \mathbf{v}_{i+1}\).
+        !!  `index` is set to 0 if \(y \lt \mathbf{v}_{1}\).
+        !!  `index` is set to `size(vector)` if \(y \geq \mathbf{v}_{\texttt{size(vector)}}\)
+
+        integer :: index_u
+        integer :: index_l
+
+        if (y .lt. vector(1)) then
+            index = 0
+            return
+        end if
+        if (y .ge. vector(size(vector))) then
+            index = size(vector)
+            return
+        end if
+
+        index_u   = size(vector)
+        index_l   = 1
+        index = ishft(index_u + index_l, -1)
+        do while ( (index_u - index_l) .gt. 1)
+            if (y .ge. vector(index)) then
+                index_l   = index
+            else
+                index_u   = index
+            end if
+            index = ishft(index_u + index_l, -1)
+        end do
+    end function binsearch
+end module re2often_utils
---
+++ test/re2often_utils_suite.f90
@@ -0,0 +1,82 @@
+! SPDX-License-Identifier: GPL-3.0-or-later
+! Copyright (C) 2024  Marco Origlia
+
+!    This program is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+
+!    This program is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+
+!    You should have received a copy of the GNU General Public License
+!    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+module re2often_utils_suite
+    !! author: Marco Origlia
+    !! license: GPL-3.0-or-later
+    !!
+    !! Test suite for the utilities
+    use iso_fortran_env, only: dp => real64
+    use stdlib_stats_distribution_normal, only: cdf_normal
+    use re2often_utils
+    use testdrive, only : new_unittest, unittest_type, error_type, check
+    implicit none
+
+    private
+
+    public :: collect_suite
+
+contains
+
+    subroutine collect_suite(testsuite)
+        type(unittest_type), allocatable, intent(out) :: testsuite(:)
+
+        testsuite = [&
+            new_unittest("Binary search", test_binsearch)]
+    end subroutine collect_suite
+
+
+    subroutine test_binsearch(error)
+        type(error_type), intent(out), allocatable :: error
+
+        real(dp) :: arr1(1)
+        real(dp) :: arr2(2)
+        real(dp) :: arr31(31)
+        integer  :: i
+
+        arr1(1) = 3d0
+        call check(error, binsearch(arr1, 2d0), 0)
+        if (allocated(error)) return
+        call check(error, binsearch(arr1, 3d0), 1)
+        if (allocated(error)) return
+        call check(error, binsearch(arr1, 3.1d0), 1)
+        if (allocated(error)) return
+
+        arr2 = real([-2, 4], dp)
+        call check(error, binsearch(arr2, -3d0), 0)
+        if (allocated(error)) return
+        call check(error, binsearch(arr2, real(-2, dp)), 1)
+        if (allocated(error)) return
+        call check(error, binsearch(arr2, 3.1d0), 1)
+        if (allocated(error)) return
+        call check(error, binsearch(arr2, real(4, dp)), 2)
+        if (allocated(error)) return
+        call check(error, binsearch(arr2, 9d0), 2)
+        if (allocated(error)) return
+
+        arr31 = [(real(i, dp), i = 1, 31)]
+        call check(error, binsearch(arr31, -3d0), 0)
+        if (allocated(error)) return
+        call check(error, binsearch(arr31, real(1, dp)), 1)
+        if (allocated(error)) return
+        call check(error, binsearch(arr31, 3.1d0), 3)
+        if (allocated(error)) return
+        call check(error, binsearch(arr31, real(4, dp)), 4)
+        if (allocated(error)) return
+        call check(error, binsearch(arr31, 27.5d0), 27)
+        if (allocated(error)) return
+        call check(error, binsearch(arr31, 32d0), 31)
+    end subroutine test_binsearch
+end module re2often_utils_suite
