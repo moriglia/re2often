31a29b5c93d50549e4a24da7f59a546d63d73658
Marco Origlia <marco.origlia@santannapisa.it>
2024-12-27 18:31:26 +0100

    Fix setup of counter buffers in reverse reconciliation program

--- programs/reverse_reconciliation.f90
+++ programs/reverse_reconciliation.f90
@@ -193,6 +193,10 @@ program reverse_reconciliation
     allocate(f_err(nsnr)[*])
     allocate(f_cnt(nsnr)[*])
 
+    b_err(:) = 0
+    f_err(:) = 0
+    f_cnt(:) = 0
+
     critical
         call from_file(file=tanner_file, into=edge_definition, header=.true.)
     end critical
