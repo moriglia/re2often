687bd35ffc75e0a0ff9a3eca40b99253c88a2207
Marco Origlia <marco.origlia@santannapisa.it>
2025-02-26 14:33:57 +0100

    Add program for MI calculation

---
+++ programs/mutual_information.f90
@@ -0,0 +1,213 @@
+! SPDX-License-Identifier: GPL-3.0-or-later
+! Copyright (C) 2025  Marco Origlia
+
+!    This program is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+
+!    This program is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+
+!    You should have received a copy of the GNU General Public License
+!    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+program mutual_information
+    !! author: Marco Origlia
+    !! license: GPL-3.0-or-later
+    !!
+    !! Numerically evaluate the Mutual Information of various schemes
+    use iso_fortran_env, only: lock_type, dp => real64
+    use io_fortran_lib, only: from_file, to_file
+    ! use stdlib_random, only: stdlib_random_seed => random_seed
+    ! use stdlib_stats_distribution_normal, only: rvs_normal
+    ! use re2often_noise_mapper, only: TNoiseMapper
+    use re2often_noisemapper
+    use re2often_utils, only: save_data, make_directory_and_file_name
+    use forbear, only: bar_object
+    use re2often_mi ! defines a noisemapper_type object
+    implicit none
+
+    ! +---------------------+
+    ! | Command line inputs |
+    ! +---------------------+
+    integer :: argc
+    character(len=500), allocatable :: argv(:)
+
+    character(250) :: output_root
+    character(250) :: output_dir
+    character(250) :: output_name
+    character(5)   :: header(3)
+    integer        :: io
+    double precision :: snr(2)         ! Signal to Noise Ratio in dB
+    integer :: nsnr           ! Number of SNR points
+    integer :: bps            ! Bits per symbol
+    logical :: isReverse      ! Whether to calculate the M.I. for the reverse reconciliation
+    logical :: isHard         ! Whether to perform hard reverse reconciliation
+    logical :: uniform_th     ! Whether to set thresholds for uniform output symbols
+
+    ! +-------------+
+    ! | Output data |
+    ! +-------------+
+    double precision, allocatable, target :: outdata(:,:)[:]
+    ! double precision, pointer             :: snrdb(:)
+    ! double precision, pointer             :: snrdb_scaled(:)
+    ! double precision, pointer             :: I(:)
+
+    logical, allocatable                  :: snr_done(:)[:]
+
+    ! +---------------------+
+    ! | Image-specific data |
+    ! +---------------------+
+    integer :: me, n_im
+
+    type(bar_object) :: progress_bar
+
+    type(lock_type) :: lck[*]
+
+
+    ! generic iteration variables
+    integer :: i_snr, ii
+
+    if (this_image() == 1) then
+        print *, " +----------------------------+"
+        print *, " | Mutual Information program |"
+        print *, " +----------------------------+"
+    end if
+
+    argc = command_argument_count()
+    allocate(argv(argc))
+
+    do ii = 1, argc
+        call get_command_argument(ii, argv(ii))
+    end do
+
+
+    ! +--------------------+
+    ! | Set default values |
+    ! +--------------------+
+    nsnr = 11
+    snr  = [3.5d0, 4d0]
+    bps  = 2
+    isReverse = .false.
+    isHard = .false.
+    uniform_th = .false.
+
+    ii = 1
+    do while(ii <= argc)
+        if (argv(ii) == "--nsnr") then
+            read(argv(ii+1),*) nsnr
+            ii = ii + 2
+            ! print *, "nsnr", nsnr
+        elseif (argv(ii) == "--snr") then
+            read(argv(ii+1), *) snr(1)
+            read(argv(ii+2), *) snr(2)
+            ii = ii+3
+            ! print *, "snr", snr
+        elseif (argv(ii) == "--bps") then
+            read(argv(ii+1),*) bps
+            ii = ii + 2
+            ! print *, "bps", bps
+        elseif (argv(ii) == "--outdir") then
+            call get_command_argument(ii+1, output_root)
+            ii = ii + 2
+        elseif (argv(ii) == "-h") then
+            isHard = .true.
+            ii = ii + 1
+        elseif (argv(ii) == "-r") then
+            isReverse = .true.
+            ii = ii + 1
+        elseif (argv(ii) == "-u") then
+            uniform_th = .true.
+            ii = ii + 1
+        else
+            print *, "Unrecognized argument: ", argv(ii)
+            stop
+        end if
+    end do
+
+    me   = this_image()
+    n_im = num_images()
+
+    ! +----------------------------------------+
+    ! | Allocation of simulation result arrays |
+    ! +----------------------------------------+
+    allocate(snr_done(nsnr)[*])
+    allocate(outdata(nsnr, 3)[*])
+    if (me==1) then
+        snr_done(:) = .false.
+        outdata(:, 1) = [(snr(1) + real(ii, dp)*(snr(2)-snr(1))/real(nsnr - 1, dp), ii = 0, nsnr-1)]
+        outdata(:,2:) = 0
+    end if
+    ! snrdb        => outdata(:, 1)[1]
+    ! snrdb_scaled => outdata(:, 2)[1]
+    ! I            => outdata(:, 3)[1]
+
+    nm = noisemapper_create(bps)
+    if (isReverse .and. (.not. isHard)) then
+        call noisemapper_set_monotonicity(nm)
+    end if
+
+    i_snr = 1
+    loop_snr : do while (i_snr .le. nsnr)
+        lock(lck[1])
+        do while(snr_done(i_snr)[1])
+            i_snr = i_snr + 1
+            if (i_snr .gt. nsnr) then
+                unlock(lck[1])
+                exit loop_snr
+            end if
+        end do
+        snr_done(i_snr)[1] = .true.
+        unlock(lck[1])
+
+        if (isReverse) then
+            if (isHard) then
+                if (uniform_th) then
+                    outdata(i_snr, 3)[1] = I_hard_reverse_uniform_output_th(outdata(i_snr, 1)[1])
+                else
+                    outdata(i_snr, 3)[1] = I_hard_reverse_equidistant_th(outdata(i_snr, 1)[1])
+                end if
+            else
+                if (uniform_th) then
+                    outdata(i_snr, 3)[1] = I_soft_reverse_uniform_output_th(outdata(i_snr, 1)[1])
+                else
+                    outdata(i_snr, 3)[1] = I_soft_reverse_equidistant_th(outdata(i_snr, 1)[1])
+                end if
+            end if
+        else
+            outdata(i_snr, 3)[1] = I_direct(outdata(i_snr, 1)[1])
+        end if
+    end do loop_snr
+
+    ! +-----------------------------------------+
+    ! | First Image saves the simulation result |
+    ! +-----------------------------------------+
+    sync all
+
+    if (me == 1) then
+        outdata(:, 2) = outdata(:, 1) - 10*log10(outdata(:,3))
+        call make_directory_and_file_name(output_root, bps, isReverse, isHard, &
+            snr, nsnr, 0, 0, 0, 0, output_dir, output_name)
+        call execute_command_line("mkdir -p " // trim(output_dir))
+        open(newunit=io, file=trim(output_dir) // "/" // trim(output_name) // ".log", &
+            status="replace", action="write")
+
+        write(io, '(A, T16, A, T32, A)') &
+            "SNR [dB]", "Eb/N0 [dB]", "I"
+        do i_snr = 1, nsnr
+            write(io, '(f12.9, T16, f12.9, T32, E12.3E3)') &
+                outdata(i_snr, :)
+        end do
+        close(io)
+
+
+        header(1) = trim("SNR")
+        header(2) = trim("scSNR")
+        header(3) = trim("I")
+        print *, outdata(:,:)
+        call to_file(x=outdata, file=trim(output_dir)//"/"//trim(output_name)//".csv", &
+            header=header, fmt="f")
+    end if
+end program mutual_information
--- src/re2often_mi.f90
+++ src/re2often_mi.f90
@@ -20,11 +20,255 @@ module re2often_mi
     !! Mutual information functions
     use, intrinsic :: iso_c_binding
     use re2often_noisemapper
-    use quadpack, only: dqags
-
-
+    use quadpack, only: dqags, dqagi
     implicit none
+
+    private
+    public :: I_soft_reverse_equidistant_th, I_soft_reverse_uniform_output_th
+    public :: I_hard_reverse_equidistant_th, I_hard_reverse_uniform_output_th
+    public :: I_direct
+
+    type(noisemapper_type) :: nm
+
+    public :: nm
+
+    real(c_double), parameter :: sq2 = sqrt(2d0)
 
 contains
 
+    real(c_double) elemental function log2(arg) result(l)
+        real(c_double), intent(in) :: arg
+
+        l = log(arg)/log(2d0)
+    end function log2
+
+    ! +-----------------------------+
+    ! | Soft Reverse Reconciliation |
+    ! +-----------------------------+
+
+    real(c_double) impure elemental function f_n_xhat_cond_x(n, xhat, x) result(pdf)
+        !! PDF of \(N, \hat{X}|X\)
+        real(c_double), intent(in) :: n
+        !! Soft metric
+        integer(c_int), intent(in) :: xhat
+        !! Bob's decided symbol (index within constellation)
+        integer(c_int), intent(in) :: x
+        !! Alice's transmitted symbol (index within constellation)
+
+        integer :: k
+        real(c_double) :: a_j, two_y_i
+
+        pdf = 0
+
+        a_j = nm%constellation(x)
+        two_y_i = 2*noisemapper_invert_soft_metric(nm, n, xhat)
+        do k = 0, nm%M-1
+            pdf = pdf + nm%probabilities(k) * &
+                exp((nm%constellation(k) - a_j)*(two_y_i - nm%constellation(k) - a_j)/nm%N0)
+        end do
+        pdf = nm%delta_Fy(xhat) / pdf
+    end function f_n_xhat_cond_x
+
+
+    real(c_double) function f_soft_reverse(n) result(f)
+        !! Argument of the finite integral
+        real(c_double), intent(in) :: n
+        !! Soft metric
+
+        integer :: j, i
+        real(c_double), allocatable :: f_n_xhat_cond_x_array(:,:)
+        real(c_double) :: log2_f_n_cond_x
+
+        allocate(f_n_xhat_cond_x_array(0:nm%M-1, 0:nm%M-1))
+        ! 1st index : X
+        ! 2nd index : Xhat
+
+        do j = 0, nm%M-1
+            f_n_xhat_cond_x_array(j, :) = f_n_xhat_cond_x(n, [(i, i=0, nm%M-1)], j)
+        end do
+
+        f = 0
+
+        do j = 0, nm%M-1 ! X index
+            log2_f_n_cond_x = log2(sum(f_n_xhat_cond_x_array(j, :)))
+            do i = 0, nm%M-1 ! Xhat index
+                f = f + nm%probabilities(j) * f_n_xhat_cond_x_array(j, i) * &
+                    (log2(f_n_xhat_cond_x_array(j, i)) - log2_f_n_cond_x)
+            end do
+        end do
+    end function f_soft_reverse
+
+    real(c_double) function H_Xhat(nm) result(H)
+        !! Entropy of the output symbols
+        type(noisemapper_type), intent(in) :: nm
+        !! Noise mapper
+
+        H = sum(nm%delta_Fy * log2(nm%delta_Fy))
+    end function H_Xhat
+
+
+    real(c_double) function I_soft_reverse_equidistant_th(snrdb) result(I)
+        !! Mutual information of the soft reverse reconciliation scheme
+        real(c_double), intent(in) :: snrdb
+        !! SNR [dB] at which to calculate the mutual information
+
+        real(c_double) :: Abserr
+        integer :: Neval, Ier, Limit, Lenw, Last
+
+        integer :: Iwork(100)
+        real(c_double) :: Work(400)
+        Limit = 100
+        Lenw = 400
+
+        call noisemapper_update_N0_from_snrdb(nm, snrdb)
+        call noisemapper_set_y_thresholds(nm)
+        call noisemapper_set_Fy_grids(nm)
+
+        call dqags(f_soft_reverse, 0d0, 1d0, 1d-12, 1d-6, &
+            I, Abserr, Neval, Ier, &
+            Limit, Lenw, Last, Iwork, Work)
+
+        if (Ier /= 1) then
+            print '("Error at ", f10.3, " [dB]: error ", i1)', snrdb, Ier
+        end if
+
+        I = I + H_Xhat(nm)
+    end function I_soft_reverse_equidistant_th
+
+
+    real(c_double) function I_soft_reverse_uniform_output_th(snrdb) result(I)
+        !! Mutual information of the soft reverse reconciliation scheme
+        real(c_double), intent(in) :: snrdb
+        !! SNR [dB] at which to calculate the mutual information
+
+        real(c_double) :: Abserr
+        integer :: Neval, Ier, Limit, Lenw, Last
+
+        integer :: Iwork(100)
+        real(c_double) :: Work(400)
+        Limit = 100
+        Lenw = 400
+
+        call noisemapper_update_N0_from_snrdb(nm, snrdb)
+        call noisemapper_set_y_thresholds_uniform(nm)
+        call noisemapper_set_Fy_grids(nm)
+
+        call dqags(f_soft_reverse, 0d0, 1d0, 1d-12, 1d-6, &
+            I, Abserr, Neval, Ier, &
+            Limit, Lenw, Last, Iwork, Work)
+
+        if (Ier /= 1) then
+            print '("Error at ", f10.3, " [dB]: error ", i1)', snrdb, Ier
+        end if
+
+        I = I + H_Xhat(nm)
+    end function I_soft_reverse_uniform_output_th
+
+    ! +-----------------------------+
+    ! | Hard reverse reconciliation |
+    ! +-----------------------------+
+
+    real(c_double) function H_Xhat_cond_X(nm) result(H)
+        !! conditional entropy \(H(\hat{X}|X)\)
+        type(noisemapper_type), intent(in) :: nm
+        !! Noise mapper
+
+        integer :: i, j
+        H = 0
+
+        do i = 0, nm%M-1
+            do j = 0, nm%M-1
+                H = nm%probabilities(j) * nm%fwd_probabilities(j, i) * &
+                    log2(nm%fwd_probabilities(j, i))
+            end do
+        end do
+        H = - H
+    end function H_Xhat_cond_X
+
+
+    real(c_double) function I_hard_reverse_equidistant_th(snrdb) result (I)
+        !! Mutual information of the discrete Input and Output channel
+        real(c_double), intent(in) :: snrdb
+        !! SNR [dB] at which to evaluate the Mutual information
+
+        call noisemapper_update_N0_from_snrdb(nm, snrdb)
+        call noisemapper_set_y_thresholds(nm)
+        call noisemapper_update_hard_reverse_tables(nm)
+
+        I = H_Xhat(nm) - H_Xhat_cond_X(nm)
+    end function I_hard_reverse_equidistant_th
+
+
+    real(c_double) function I_hard_reverse_uniform_output_th(snrdb) result (I)
+        !! Mutual information of the discrete Input and Output channel
+        real(c_double), intent(in) :: snrdb
+        !! SNR [dB] at which to evaluate the Mutual information
+
+        call noisemapper_update_N0_from_snrdb(nm, snrdb)
+        call noisemapper_set_y_thresholds_uniform(nm)
+        call noisemapper_update_hard_reverse_tables(nm)
+
+        I = H_Xhat(nm) - H_Xhat_cond_X(nm)
+    end function I_hard_reverse_uniform_output_th
+
+
+    ! +----------------------------+
+    ! ! Soft direct reconciliation |
+    ! +----------------------------+
+
+    real(c_double) function f_integrand_GH(x) result(f)
+        !! Function to be integrated with the Gauss-Hermite quadrature rule
+        real(c_double), intent(in) :: x
+        !! Scaled Y
+
+        integer :: j, k
+        real(c_double) :: log_arg, a_j, a_k
+        f = 0
+
+        do j = 0, nm%M-1
+            log_arg = 0
+            a_j = nm%constellation(j)
+            do k = 0, nm%M-1
+                a_k = nm%constellation(k)
+                log_arg = log_arg + nm%probabilities(k) * &
+                    exp((a_k-a_j)*(a_j-a_k + 2*x*sq2*nm%sigma)/nm%N0)
+            end do
+            f = f + nm%probabilities(j) * log2(log_arg)
+        end do
+        f = f / sqrt(acos(-1d0))
+    end function f_integrand_GH
+
+
+    real(c_double) function f_integrand_soft_direct(x) result(f)
+        !! Integrand function for soft direct reconciliation
+        real(c_double), intent(in) :: x
+        !!
+
+        f = - exp(-x**2) * f_integrand_GH(x)
+    end function f_integrand_soft_direct
+
+
+    real(c_double) function I_direct(snrdb) result(I)
+        !! Mutual information of the direct reconciliation scheme
+        real(c_double), intent(in) :: snrdb
+        !! SNR [dB] at which to calculate the mutual information
+
+        real(c_double) :: Abserr
+        integer :: Neval, Ier, Limit, Lenw, Last
+
+        integer :: Iwork(100)
+        real(c_double) :: Work(400)
+        Limit = 100
+        Lenw = 400
+
+        call noisemapper_update_N0_from_snrdb(nm, snrdb)
+
+        call dqagi(f_integrand_soft_direct, 0d0, 2, 1d-12, 1d-6, &
+            I, Abserr, Neval, Ier, &
+            Limit, Lenw, Last, Iwork, Work)
+
+        if (Ier /= 0) then
+            print '("Error at ", f10.3, " [dB]: error ", i1)', snrdb, Ier
+        end if
+    end function I_direct
 end module re2often_mi
