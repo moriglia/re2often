d2c1ebbc75e7b30e96146c5c137f2d27a9411c2c
Marco Origlia <marco.origlia@santannapisa.it>
2025-04-12 13:41:13 +0200

    Update manifest with self-contained opencoarrays package definition

--- manifest.scm
+++ manifest.scm
@@ -1,8 +1,44 @@
-;; What follows is a "manifest" equivalent to the command line you gave.
-;; You can store it in a file that you may then pass to any 'guix' command
-;; that accepts a '--manifest' (or '-m') option.
+(use-modules
+ (guix packages)
+ (guix profiles)
+ (guix git-download)
+ (guix build-system cmake)
+ (guix licenses)
+ (gnu packages gcc)
+ (gnu packages cmake)
+ (gnu packages mpi))
+
+(define open-coarrays
+  (package
+   (name "open-coarrays")
+   (version "2.10.2")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/sourceryinstitute/OpenCoarrays.git")
+           (commit version)))
+     (sha256
+      (base32
+       "1qcbqmj1kpd8lhjva6sb7250rhjw4s6ly553gssic2zc6w1zzmg7"))))
+   (build-system cmake-build-system)
+   (arguments
+    `(#:tests? #f))
+   (inputs
+    `(("gfortran-toolchain" ,gfortran)
+      ("mpi" ,openmpi)))
+   (synopsis "Open Coarrays library")
+   (description
+    "OpenCoarrays is an open-source software project that produces an application
+binary interface (ABI) used by the GNU Compiler Collection (GCC) Fortran
+front-end to build executable programs that leverage the parallel programming
+features of Fortran 2018.")
+   (home-page "https://www.opencoarrays.org")
+   (license bsd-3)))
+
+
 
 (specifications->manifest
-  (list "gfortran-toolchain"
-        "openmpi"
-        "open-coarrays"))
+ '("gfortran-toolchain"
+   "openmpi"
+   "open-coarrays"))
