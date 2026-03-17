c0f831f97eb8f52abfe6243bb6ae2f5f2d2ecff4
Marco Origlia <marco.origlia@santannapisa.it>
2026-03-17 11:36:15 +0100

    Add count to confiturations_list program

--- programs/configurations_list.f90
+++ programs/configurations_list.f90
@@ -1,5 +1,5 @@
 ! SPDX-License-Identifier: GPL-3.0-or-later
-! Copyright (C) 2025  Marco Origlia
+! Copyright (C) 2025--2026  Marco Origlia
 
 !    This program is free software: you can redistribute it and/or modify
 !    it under the terms of the GNU General Public License as published by
@@ -20,90 +20,92 @@ program configurations_list
     !! List one representative for each configurations class
     implicit none
 
-    integer :: argc, ii, bps, M, other
+    integer(8) :: ii, bps, M, other, count
+    integer ::argi, argc
     character(len=500), allocatable :: argv(:)
     logical :: vFlag
 
     argc = command_argument_count()
     allocate(argv(argc))
 
-    do ii = 1, argc
-        call get_command_argument(ii, argv(ii))
+    do argi = 1, argc
+        call get_command_argument(argi, argv(argi))
     end do
 
     bps = 2
     M = 4
     vFlag = .false.
 
-    ii = 1
-    do while (ii .le. argc)
-        if (argv(ii) == "--bps") then
-            ii = ii + 1
-            read(argv(ii),*) bps
-        elseif (argv(ii) == "--verbose") then
+    argi = 1
+    do while (argi .le. argc)
+        if (argv(argi) == "--bps") then
+            argi = argi + 1
+            read(argv(argi),*) bps
+        elseif (argv(argi) == "--verbose") then
             print *, "Verbose output"
             vFlag = .true.
         else
             print *, "Unrecognized argument"
             stop
         end if
-        ii = ii + 1
+        argi = argi + 1
     end do
 
 
     M = ishft(1, bps)
+    count = 0
     cfg_loop: do ii = 0, ishft(1, M) - 1
 
         other = flip(ii, M)
         if (other .lt. ii) then
             call display_verbose(vFlag, "Flipped", other, ii)
-            goto 100
+            cycle cfg_loop
         end if
 
         other = reverse(ii, M)
         if (other .lt. ii) then
             call display_verbose(vFlag, "Reverse", other, ii)
-            goto 100
+            cycle cfg_loop
         end if
 
         other = mirror(ii, M)
         if (other .lt. ii) then
             call display_verbose(vFlag, "Mirrord", other, ii)
-            goto 100
+            cycle cfg_loop
         end if
 
         print '(I0)', ii
-
-100     continue
+        count = count + 1
     end do cfg_loop
 
+    if (vFlag) print '("Count: ", I0)', count
 contains
-    integer function flip(ii, M) result(other)
-        integer, intent(in) :: ii
-        integer, intent(in) :: M
+    integer(8) function flip(ii, M) result(other)
+        integer(8), intent(in) :: ii
+        integer(8), intent(in) :: M
 
         other = ishft(1, M) - 1  ! all 1s
         other = ieor(other, ii) ! bitwise xor
     end function flip
 
 
-    integer function reverse(ii, M) result(other)
-        integer, intent(in) :: ii
-        integer, intent(in) :: M
+    integer(8) function reverse(ii, M) result(other)
+        integer(8), intent(in) :: ii
+        integer(8), intent(in) :: M
 
-        integer :: pos
+        integer(8) :: pos
 
         other = 0
 
         do pos = 0, M-1
-            other = ior(other, ishft(iand(ishft(ii, -(M-1-pos)), 1), pos))
+            other = ior(other, ishft(iand(ishft(ii, -(M-1-pos)), int(1, kind=8)), pos))
         end do
     end function reverse
 
 
-    integer function mirror(ii, M) result(other)
-        integer, intent(in) :: ii
-        integer, intent(in) :: M
+    integer(8) function mirror(ii, M) result(other)
+        integer(8), intent(in) :: ii
+        integer(8), intent(in) :: M
 
         other = flip(reverse(ii, M), M)
     end function mirror
@@ -112,8 +114,8 @@ contains
     subroutine display_verbose(flag, a, b, c)
         logical, intent(in) :: flag
         character(len=*), intent(in) :: a
-        integer, intent(in) :: b
-        integer, intent(in) :: c
+        integer(8), intent(in) :: b
+        integer(8), intent(in) :: c
 
         if (flag) then
             print '(A, T10, I0, T20, I0)', a, b, c
